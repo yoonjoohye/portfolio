9c762ecea85d25220f9e1a71752c8deb5d1afbb2
yoonjoohye <67yjh00@gmail.com>
2020-07-22 00:03:05 +0900

    [fix] safari css 수정

--- src/views/Greeting.vue
+++ src/views/Greeting.vue
@@ -1,10 +1,9 @@
 <template>
-    <section class="fixed z-0 bg-image w-full h-64 sm:h-40 flex flex-col justify-center items-center text-center text-white">
+    <section class="fixed z-0 bg-image w-full h-64 sm:h-40 flex flex-col justify-center items-center">
 
         <img class="absolute left-10 bottom-0 w-32 h-32 sm:w-20 sm:h-20" src="../assets/images/logo/gooddoc.png"/>
 
-        <div class="text-6xl font-normal sm:text-3xl">윤주혜</div>
-
+        <div class="text-white text-6xl font-bold sm:text-3xl">윤주혜</div>
         <div class="absolute right-10 text-left text-blue-800 only-pc">
             <div class="text-6xl md:text-3xl font-bold italic">
                 # 변화는 있지만,<br/>
